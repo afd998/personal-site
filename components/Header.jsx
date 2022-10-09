f22354333a9adcb6fc8fb6657dc257f589547a1d
Atticus Deutsch <afd998@gmail.com>
2022-10-08 23:07:21 -0500

    fixed header text

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import Link from "next/link";
 import DarkModeIcon from "../public/icons/DarkMode.svg";
 import Switch from "./Switch";
+import { useRouter } from "next/router";
+
 function Header() {
+  const { asPath, pathname } = useRouter();
   return (
     <div
       className={` z-10 text-3xl font-bold underline  dark:decoration-gray-900  flex justify-between pr-3 pl-3 py-2`}
     >
       <Link href="/">
         <h1 className=" inline hover:cursor-pointer dark:text-gray-900 ">
-          {"< All Projects"}{" "}
+          {pathname === "/" ? "Atticus Deutsch" : "< All Projects"}
         </h1>
       </Link>
       {/* <div className=" flex justify-evenly items-center space-x-2">
